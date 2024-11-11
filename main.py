59ea70187aa0595efbb4ec2f7f5e8aa69c92ba92
POTATOPROGAMMER <codercode1010@gmail.com>
2024-11-11 17:59:27 +0400

    commit1

---
+++ main.py
@@ -0,0 +1,14 @@
+zayed="17/12/2013"
+print ("rons birthday in on", zayed)
+
+greg="18/7/2011"
+print ("greg's birthday is on", greg)
+
+mark="19/8/2009"
+print ("mark's birthday is on", mark)
+
+jack="31/5/2006"
+print ("jacks's birthday is on", jack)
+
+hailey="11/12/2012"
+print ("hailey's birthday is on", hailey)
